7a6ebff37666eb3e620253da9a35354cf4f9aa3c
sergio garcia <sergiogarciamf@gmail.com>
2019-05-03 19:09:39 -0400

    Define naming convention

--- src/modcell.c
+++ src/modcell.c
@@ -1,41 +1,41 @@
 #include <stdio.h>
 #include <string.h>
+#include <dirent.h>
 #include "mcutils.h"
 #include "modcell.h"
-#include <dirent.h>
 
-MCprob readproblem(const char *problemdir);
-bool isnotcandidate(struct file ncandfile, const char *rxnid);
-void loadparams(MCprob *mcprob, const char *filepath);
+MCproblem readproblem(const char *problem_dir);
+bool is_not_candidate(struct file ncandfile, const char *rxnid);
+void load_parameters(MCproblem *mcp, const char *filepath);
 
-MCprob
-readproblem(const char *problemdir){
-    MCprob mcprob = {NULL};
-    DIR *d;
+MCproblem
+read_problem(const char *problem_dir){
+    MCproblem mcp = {NULL};
+    DIR *d; //TODO: Move directory reading as an auxiliary function in mcutils.h
     struct dirent *dir;
 
-    d = opendir(problemdir);
+    d = opendir(problem_dir);
     if (!d) {
-        fprintf (stderr, "error: opening problem directory: '%s'.", problemdir);
+        fprintf (stderr, "error: opening problem directory: '%s'.", problem_dir);
 	exit(-1);
     }
 
     /* Obtain basic info */
-    mcprob.nmodels = 0;
+    mcp.n_models = 0;
     while ((dir = readdir(d)) != NULL) {
         /* Determine number of models */
         if(strcmp( getExt (dir->d_name), ".mps") == 0){
-    	    mcprob.nmodels++;
+    	    mcp.n_models++;
         }
         if(strcmp( dir->d_name, "cand") == 0){
             /* Determine deletion candidates */
     	    char full_path[256];
-    	    strcpy(full_path, problemdir);
+    	    strcpy(full_path, problem_dir);
     	    strcat(full_path, dir->d_name);
     	    struct file myfile = readFile(full_path);
-            mcprob.individual2id = myfile.array;
-            mcprob.nvars = myfile.nlines;
-    	    //freeFile(myfile); Cannot free since it contains information in mcprob!
+            mcp.individual2id = myfile.array;
+            mcp.n_vars = myfile.nlines;
+    	    //freeFile(myfile); Cannot free since it contains information in mcp! See strdup()
     	}
     }
     closedir(d);
@@ -45,51 +45,51 @@ readproblem(const char *problemdir){
     glp_init_smcp(&param);
     param.msg_lev = GLP_MSG_ERR;
 
-    mcprob.Ps = malloc(mcprob.nmodels * sizeof(*mcprob.Ps)); // TODO: why does changing nmodels to lower value still work?
+    mcp.Ps = malloc(mcp.n_models * sizeof(*mcp.Ps));
 
-    d = opendir(problemdir);
+    d = opendir(problem_dir);
     int i=0;
     while ((dir = readdir(d)) != NULL) {
         if(strcmp( getExt (dir->d_name), ".mps") == 0){
     	    char full_path[256];
-    	    strcpy(full_path, problemdir);
+    	    strcpy(full_path, problem_dir);
     	    strcat(full_path, dir->d_name);
-            mcprob.Ps[i] = glp_create_prob();
-            glp_read_mps(mcprob.Ps[i], GLP_MPS_FILE, NULL, full_path); // TODO: Silence this or redirect to log
+            mcp.Ps[i] = glp_create_prob();
+            glp_read_mps(mcp.Ps[i], GLP_MPS_FILE, NULL, full_path); // TODO: Silence this or redirect to log
             /* Calculate basis and solve so subsequent computations are warm-started */
-            glp_adv_basis(mcprob.Ps[i], 0);
-            glp_simplex(mcprob.Ps[i], &param);
+            glp_adv_basis(mcp.Ps[i], 0);
+            glp_simplex(mcp.Ps[i], &param);
             i++;
         }
     }
     closedir(d);
 
     /* Create maps between indvidual and bounds */
-    mcprob.individual2glp = malloc(mcprob.nmodels * sizeof(*mcprob.individual2glp));
-    for (int k =0; k < mcprob.nmodels; k++){
-        mcprob.individual2glp[k] = malloc(mcprob.nvars * sizeof(**mcprob.individual2glp));
+    mcp.individual2glp = malloc(mcp.n_models * sizeof(*mcp.individual2glp));
+    for (int k=0; k < mcp.n_models; k++){
+        mcp.individual2glp[k] = malloc(mcp.n_vars * sizeof(**mcp.individual2glp));
 
         char model_path[256];
-    	strcpy(model_path, problemdir);
-    	strcat(strcat(model_path, glp_get_prob_name(mcprob.Ps[k])), ".ncand");
+    	strcpy(model_path, problem_dir);
+    	strcat(strcat(model_path, glp_get_prob_name(mcp.Ps[k])), ".ncand");
     	struct file ncandfile = readFile(model_path);
 
-      	glp_create_index(mcprob.Ps[k]);
-        for(int j=0; j < mcprob.nvars; j++){
-            if(isnotcandidate(ncandfile, mcprob.individual2id[j]))
-                mcprob.individual2glp[k][j] = NOT_CANDIDATE;
+      	glp_create_index(mcp.Ps[k]);
+        for(int j=0; j < mcp.n_vars; j++){
+            if(is_not_candidate(ncandfile, mcp.individual2id[j]))
+                mcp.individual2glp[k][j] = NOT_CANDIDATE;
             else
-                mcprob.individual2glp[k][j] = glp_find_col(mcprob.Ps[k], mcprob.individual2id[j]); // If glp fails to find the column it will error
+                mcp.individual2glp[k][j] = glp_find_col(mcp.Ps[k], mcp.individual2id[j]); // If glp fails to find the column it will error
         }
-      	glp_delete_index(mcprob.Ps[k]);
+      	glp_delete_index(mcp.Ps[k]);
     	freeFile(ncandfile);
     }
 
-return mcprob;
+return mcp;
 }
 
 bool
-isnotcandidate(struct file ncandfile, const char *rxnid){
+is_not_candidate(struct file ncandfile, const char *rxnid){
     for (int i=0; i < ncandfile.nlines; i++)
         if(!strcmp(ncandfile.array[i], rxnid))
                 return true;
@@ -97,13 +97,13 @@ isnotcandidate(struct file ncandfile, const char *rxnid){
 }
 
 #define READPARAM(id, type) \
-    if (fscanf(fp, #id"=%"#type"\n", &mcprob->id) !=1){ \
+    if (fscanf(fp, #id"=%"#type"\n", &mcp->id) !=1){ \
         fprintf (stderr, "error: parsing parameter'%s'\n", #id); \
 	exit(-1); \
     } // print parameter here if interested
 
 void
-loadparams(MCprob *mcprob, const char *filepath){
+load_parameters(MCproblem *mcp, const char *filepath){
     FILE *fp = NULL;
 
     /* open file for reading   */
@@ -111,34 +111,33 @@ loadparams(MCprob *mcprob, const char *filepath){
         fprintf (stderr, "error: file open failed '%s'\n", filepath);
 	exit(-1);
     }
-    READPARAM(objective,s);
+    READPARAM(objective_type,s);
     READPARAM(alpha,d);
     READPARAM(beta,d);
-    READPARAM(popsize,d);
-    READPARAM(ngen,d);
-    READPARAM(ncores,d);
-    READPARAM(seed,d);
+    READPARAM(population_size,d);
+    READPARAM(n_generations,d);
+    READPARAM(n_cores,d);
+    READPARAM(seed,u);
     if (fp) fclose (fp);
 }
 
-population
-loadpopulation(MCprob *mcprob, const char *inpoppath){
-    // If inpoppath is NULL generate a pop randomly
 
-    // Deal with discrepancies between specified popsize and file popsize. Or discrepancies between design variables (e.g. b=0 vs b=1?)
+//void
+//writepopulation(MCproblem *mcp, Individual *outpop, const char *out_population_path){
+//}
+//
+//void
+//set_initial_population(MCproblem *mcp, Individual **pop, char *population_path){
+//};
+//
+//void
+//set_random_population(MCproblem *mcp, Individual **pop){
+//}
 
-    // Calculate pop objective values?
-}
-
-void
-writepopulation(MCprob *mcprob, population *outpop, const char *outpoppath){
-}
-
-// population runmoea(MCprob *mcprob) // define on separate file
+// population runmoea(MCproblem *mcp) // define on separate file
 
 int
 main (int argc, char **argv) {
-    //TODO: Print only critical info to stdout, print detailed info (including whatever is printed to stdout) to an automatically generated log file. (Or maybe just print everything to log file?)
 
 /* Parse  input */
 if (argc < 3){
@@ -146,24 +145,27 @@ if (argc < 3){
         return(1);
     }
 
-MCprob mcprob = readproblem(argv[1]);
-loadparams(&mcprob, argv[2]);
+MCproblem mcp = read_problem(argv[1]);
+load_parameters(&mcp, argv[2]);
+printf("--------------------------------------------------\n");
+
+/* Seed global RNG */
+pcg32_srandom(mcp.seed, 54u);
 
 /* Intialize population */
+// allocate
 if (argc == 4){
     char pop_path[256];
-    printf("Initial population path: %s \n", argv[3]);
+    printf("Initial population path: %s\n", argv[3]);
     strcpy(pop_path, argv[3]);
-    //inipop = loadpopulation(&mcprob, argv[1]);
+    // set population to file read
 }
 else{
     printf("Initial population not specified\n");
-    //inipop = loadpopulation(&mcprob, NULL);
+    // set random population
 }
 
 /* Run */
-// population pop = runmoea(&mctype, &inipop);
-// writepop(mctype, pop);
 
 return(0);
 }
--- src/modcell.h
+++ src/modcell.h
@@ -1,52 +1,57 @@
-#define NOT_CANDIDATE -1
-
 #include <stdbool.h>
 #include <glpk.h>
+#include "pcg_basic.h"
+
+#define NOT_CANDIDATE -1
+
+//extern pcg32_random_t rng;
+
+/* Main structures */
 
 typedef struct
 {
 	// modcell
 	bool *deletions;//[nbits];
-    	bool *modules;//[nmodels][nbits];
-      	glp_prob **P;//[nmodels];
+    	bool **modules;//[nmodels][nbits];
+      	glp_prob **Ps;//[nmodels];
 	// moea
 	int rank;
-	double crowd_dist;
-	double *obj;//[nmodels];
-	double *penaltyobj;//[nmodels];
+	double crowding_distance;
+	double *objectives;//[nmodels];
+	double *penalty_objectives;//[nmodels];
 
-} individual;
+} Individual;
 
-typedef struct
-{
-	individual *ind;
-} population;
+//typedef struct
+//{
+//	individual *ind;
+//} population;
 
 
 // General problem information
-typedef struct MCprob
+typedef struct MCproblem
 {
-	char *objective;
-	int alpha;
-	int beta;
-	int nmodels;
+	char *objective_type; // maybe use enum for this?
+	unsigned int alpha;
+	unsigned int beta;
+	unsigned int n_models;
       	glp_prob **Ps;//[nmodels];
 	// refbounds *rbounds; This can be added later if glp query has too much overhead
-	int **individual2glp; //[nmodels][nvars] Contains model index that individual maps to or NOT_CANDIDATE if module is fixed.
+	unsigned int **individual2glp; //[nmodels][nvars] Contains model index that individual maps to or NOT_CANDIDATE if module is fixed.
 	char **individual2id; //[nvars] Maps individual indices to reaction ID.
 	// hash archive; // maps individuals deletions and module variables into objective values
 
 	//MOEA
-	int ncores;
-    double seed;
-    int nvars;
+	unsigned int n_cores;
+    	unsigned int seed;
+    	int n_vars;
 //    int nobj; // called nmodels
-    int popsize;
-    double pcross_bin;
-    double pmut_bin;
+    	int population_size;
+    	double pcross_bin;
+    	double pmut_bin;
     //double eta_c;
     //double eta_m;
-    int ngen;
+    	int n_generations;
     int nbinmut;
     int nbincross;
     //int *nbits;
@@ -57,5 +62,12 @@ typedef struct MCprob
     //int obj3;
     //int angle1;
     //int angle2;
-} MCprob;
+} MCproblem;
+
+
+/* allocation.c */
+Individual initialize_individual(MCproblem *mcp);
+void free_individual(MCproblem *mcp, Individual *indv);
+Individual **initialize_population(MCproblem *mcp);
+void free_population(MCproblem *mcp, Individual *indv);
 
