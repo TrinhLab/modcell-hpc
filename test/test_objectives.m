5014cf982dec380e751785de8cf649c14f54829b
sergio garcia <sergiogarciamf@gmail.com>
2019-09-18 11:02:19 -0400

    Fix tests with module reactions

--- test/test_objectives.m
+++ test/test_objectives.m
@@ -58,10 +58,10 @@ for i = 1:length(T.SolutionIndex)
 	%% Parse module reactions
 	Z = false(length(prodnet.prod_id), length(prodnet.cand_ind));
 	var_module_id = T.Properties.VariableNames(contains(T.Properties.VariableNames, '_module'));
-	module_prod_id = cellfun(@(x)(strrep(x,'_module','')),var_module_id,'UniformOutput',false);
+	module_prod_id = cellfun(@(x)(strrep(x,'_module_','')),var_module_id,'UniformOutput',false);
 	for k = 1:length(prodnet.prod_id)
 		if ~isempty(intersect(module_prod_id, prodnet.prod_id{k}))
-			module_rxn = parse_list(T{i,[prodnet.prod_id{k},'_module']}{1});
+			module_rxn = parse_list(T{i,[prodnet.prod_id{k},'_module_']}{1});
 			[~,model_del_ind] = intersect(prodnet.parent_model.rxns, module_rxn,'stable');
 			[~,cand_del_ind] = intersect(prodnet.cand_ind,model_del_ind,'stable');
 			Z(k, cand_del_ind) = true;
@@ -69,7 +69,7 @@ for i = 1:length(T.SolutionIndex)
 end
 
 %% Compute Pareto front
-prodnet.set_module_and_deleted_variables(Z,y)
+prodnet.set_module_and_deleted_variables(Z, y)
 PF(i,:) = prodnet.calc_design_objectives(design_objective);
 design_vars(i).Z = Z;
 design_vars(i).y = y;
@@ -96,7 +96,7 @@ function out_prod_id = safe_prod_id(prod_id)
 out_prod_id = prod_id;
 for k=1:length(prod_id)
 	%out_prod_id{k} = strrep(out_prod_id{k}, '_', 'U');
-	if regexp(prod_id{k}, '^\d')
+    if regexp(prod_id{k}, '^\d')
 		out_prod_id{k} = ['z', prod_id{k}];
     end
 end
